784a077e0ddb35c09048595eedef2500fa694f20
oussematurki0 <oussematurki0@gmail.com>
2024-02-19 14:07:04 +0000

    commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Prints "with proper grammar, but the outcome is a piece of art, followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+  puts("\"with proper grammar, but the outcome is a piece of art,");
   return (0);
-  
+}
