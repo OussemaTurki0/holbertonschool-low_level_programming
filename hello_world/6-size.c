b81ac25db090009351c6f37f0722272aaa85fa55
oussematurki0 <oussematurki0@gmail.com>
2024-02-22 10:33:22 +0100

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,4 @@
-/*
- * File: 6-size.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
 /**
  * main - Prints the size of various types based on
  *        the computer it is compiled and run on..
@@ -18,6 +12,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 return (0);
 }
