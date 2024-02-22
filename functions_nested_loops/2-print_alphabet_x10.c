ed6b34618e0b6d68f8865a992080fa0ca7f75aae
oussematurki0 <oussematurki0@gmail.com>
2024-02-22 13:40:28 +0100

    commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  */
  void print_alphabet_x10(void)
 {
-    char alpha;
-    int i;
+char alpha;
+int i;
 
-    for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
+{
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
     {
-        for (alpha = 'a'; alpha <= 'z'; alpha++)
-        {
-            _putchar(alpha);
-        }
-        _putchar('\n');
+        _putchar(alpha);
     }
+    _putchar('\n');
+}
 }
