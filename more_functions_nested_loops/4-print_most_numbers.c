aa223254fd6f6fe6b553f297dbc1ab874c372244
oussematurki0 <oussematurki0@gmail.com>
2024-02-27 11:32:32 +0100

    commit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+*Return: void
+*/
+void print_less_numbers(void)
+
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+    if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
+}
