f8603bfbee0b2f6b8255b879a0ef5c009924d74d
oussematurki0 <oussematurki0@gmail.com>
2024-02-28 10:35:47 +0100

    commit

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,12 +5,13 @@
  * @str: pointer to the string to print
  */
 void _puts(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str(i));
-	}
-	_putchar('\n');
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
