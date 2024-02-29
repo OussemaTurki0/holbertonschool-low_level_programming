240786db541967106508c83dab35e41b20f9ab59
oussematurki0 <oussematurki0@gmail.com>
2024-02-29 10:52:57 +0100

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
-do{
+do {
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
@@ -18,6 +18,6 @@ else if (num > 0)
 break;
 } while (*s++);
 
-return (num * sign);
+return (num *sign);
 
 }
