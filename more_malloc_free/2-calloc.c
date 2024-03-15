b4ccc9d669f847e15a5bb8d7c8a6e5f9e286f21d
oussematurki0 <oussematurki0@gmail.com>
2024-03-15 10:08:46 +0100

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,17 +9,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
-	unsigned int i;
+    void *p;
+    unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
-		return (NULL);
+    if (nmemb <= 0 || size <= 0)
+        return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+    p = malloc(nmemb * size);
+    if (p == NULL)
+        return (NULL);
 
-	return (p);
+    // Use memset to initialize the allocated memory to zero
+    memset(p, 0, nmemb * size);
+
+    return (p);
 }
