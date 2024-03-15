4a5c3a1f2b0c6215809e49b7d6cfe1ef03df50af
oussematurki0 <oussematurki0@gmail.com>
2024-03-15 10:09:48 +0100

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h> // Include the necessary header file for memset
 
 /**
  * _calloc - allocates memory for an array given number of elements and size
@@ -9,18 +10,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *p;
-    unsigned int i;
+void *p;
 
-    if (nmemb <= 0 || size <= 0)
-        return (NULL);
+if (nmemb == 0 || size == 0)
+return NULL;
 
-    p = malloc(nmemb * size);
-    if (p == NULL)
-        return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return NULL;
 
-    // Use memset to initialize the allocated memory to zero
-    memset(p, 0, nmemb * size);
+memset(p, 0, nmemb * size);
 
-    return (p);
+return p;
 }
