a8433fd524c54cb7dc0cf39e651f51a79fd1f002
oussematurki0 <oussematurki0@gmail.com>
2024-03-21 00:40:15 +0100

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - Print a name from a function pointer
  * @name: char string
@@ -7,5 +8,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+	f(name);
 }
